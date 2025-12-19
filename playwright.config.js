e9ccfb4fbdff78f59543b93d8fb10fe478219abc
nickleezx <nickleezx@gmail.com>
2025-12-19 19:08:45 +0800

    fixed?

--- playwright.config.js
+++ playwright.config.js
@@ -10,7 +10,7 @@ import { defineConfig, devices } from '@playwright/test'
 // dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 const PORT = process.env.PORT || '5000'
-const BASE_URL = process.env.BASE_URL || `http://127.0.0.1:${PORT}`
+const baseURL = `http://localhost:${PORT}` // Define it once
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    baseURL: BASE_URL,
+    baseURL: baseURL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -76,7 +76,7 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npm run start-prod',
-    url: BASE_URL,
+    url: baseURL,
     timeout: 120 * 1000,
     reuseExistingServer: !process.env.CI,
     env: {
